1976423bd58809406afdb1423c6cb22f354bcacb
giuroperto <giu.roperto@gmail.com>
2021-02-26 20:54:44 -0300

    first activity layout almost done

--- Ex1_Cars/app/src/main/java/com/ctt/ex1_cars/MainActivity.kt
+++ Ex1_Cars/app/src/main/java/com/ctt/ex1_cars/MainActivity.kt
@@ -9,3 +9,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        // Create an ArrayAdapter
+//        val adapter = ArrayAdapter.createFromResource(this,
+//                R.array.city_list, android.R.layout.simple_spinner_item)
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        // Apply the adapter to the spinner
+//        spinner.adapter = adapter
+//
+//    }
+//
+//    fun getValues(view: View) {
+//        Toast.makeText(this, "Spinner 1 " + spinner.selectedItem.toString() +
+//                "\nSpinner 2 " + spinner2.selectedItem.toString(), Toast.LENGTH_LONG).show()
+//    }
+//}
