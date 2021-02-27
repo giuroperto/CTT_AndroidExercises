8c20c5dd60d6d77ecdaa71014ebb16a7c1dfbd0d
giuroperto <giu.roperto@gmail.com>
2021-02-26 21:23:42 -0300

    car activity layoyut done

---
+++ Ex1_Cars/app/src/main/java/com/ctt/ex1_cars/CarsActivity.kt
@@ -0,0 +1,11 @@
+package com.ctt.ex1_cars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cars)
+    }
+}
--- Ex1_Cars/app/src/main/java/com/ctt/ex1_cars/Vehicle.kt
+++
@@ -1,3 +0,0 @@
-package com.ctt.ex1_cars
-
-class Vehicle()
