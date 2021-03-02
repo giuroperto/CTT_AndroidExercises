a7007526c289f1ec3e1183c56f495073d8a44cc6
giuroperto <giu.roperto@gmail.com>
2021-03-02 19:09:41 -0300

    almost done mvp ex5 shopping list - still need to update when adding a new item

--- ShoppingList/app/src/main/java/com/ctt/shoppinglist/MainActivity.kt
+++ ShoppingList/app/src/main/java/com/ctt/shoppinglist/MainActivity.kt
@@ -2,10 +2,71 @@ package com.ctt.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ctt.shoppinglist.model.ShoppingItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var addBtn : Button
+    private lateinit var shopItem : EditText
+    private lateinit var shopQt : EditText
+
+    private lateinit var shoppingList : MutableList<ShoppingItem>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addBtn = findViewById(R.id.btnAdd)
+        shopItem = findViewById(R.id.edtItem)
+        shopQt = findViewById(R.id.edtQt)
+
+        shoppingList = mutableListOf<ShoppingItem>(
+                ShoppingItem("Milk", 2),
+                ShoppingItem("Chocolate", 5),
+                ShoppingItem("Orange Juice", 1),
+                ShoppingItem("Oatmeal", 2),
+                ShoppingItem("Bread", 1),
+                ShoppingItem("Butter", 1),
+                ShoppingItem("Tomatoes", 4),
+        )
+
+        val rvItems = findViewById<RecyclerView>(R.id.itemsList)
+        rvItems.adapter = ShoppingItemsAdapter(shoppingList)
+        rvItems.layoutManager = LinearLayoutManager(this)
+
+        addBtn.setOnClickListener{
+            val inputItem = shopItem.text.toString()
+            val inputQuantity = shopQt.text.toString()
+
+            if (inputItem.isEmpty()) {
+                shopItem.error = "Please inform an item"
+            }
+
+            if (inputQuantity.isEmpty()) {
+                shopQt.error = "Please inform the quantity"
+            }
+
+            if (inputItem.isNotEmpty() && inputQuantity.isNotEmpty()) {
+
+                val newItem = ShoppingItem(name = inputItem, quantity = inputQuantity.toInt())
+                addItem(newItem)
+            }
+        }
+    }
+
+    fun addItem(item: ShoppingItem) {
+
+        shoppingList.add(item)
+        showItem(item)
+    }
+
+    fun showItem(item: ShoppingItem) {
+        Toast.makeText(this, "Item ${item.name} successfully added!", Toast.LENGTH_SHORT).show()
     }
 }
--- ShoppingList/app/src/main/java/com/ctt/shoppinglist/ShoppingItemsAdapter.kt
+++ ShoppingList/app/src/main/java/com/ctt/shoppinglist/ShoppingItemsAdapter.kt
@@ -1,4 +1,32 @@
 package com.ctt.shoppinglist
 
-class ShoppingItemsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ctt.shoppinglist.model.ShoppingItem
+
+class ShoppingItemsAdapter(private val shoppingList: MutableList<ShoppingItem>) : RecyclerView.Adapter<ShoppingItemsAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val shoppingItem : TextView = view.findViewById(R.id.txtItem)
+        val shoppingQuantity : TextView = view.findViewById(R.id.txtQuantity)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.shoppingItem.text = shoppingList[position].name
+        holder.shoppingQuantity.text = shoppingList[position].quantity.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return shoppingList.size
+    }
 }
