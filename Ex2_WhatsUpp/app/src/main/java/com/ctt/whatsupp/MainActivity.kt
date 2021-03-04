d1ea78723ead3976e6a5f4ac01802c07ad6c2fc0
giuroperto <giu.roperto@gmail.com>
2021-03-04 02:21:31 -0300

    ex2 structure almost done - still need to dynamically load picture according to contact

--- Ex2_WhatsUpp/app/src/main/java/com/ctt/whatsupp/MainActivity.kt
+++ Ex2_WhatsUpp/app/src/main/java/com/ctt/whatsupp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.ctt.whatsupp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var picture: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        picture = findViewById(R.id.imgCamera)
 
 //        1. Vinculamos as fragments a viewpager
 
@@ -24,6 +33,22 @@ class MainActivity : AppCompatActivity() {
 
 //        para organizar por tabs, ir no adapter e criar um novo metodo
 
+        picture.setOnClickListener{
+            abrirCamera()
+        }
+
+    }
+
+    fun abrirCamera() {
+        val CAMERA_REQUEST_CODE = 12345
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (cameraIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        } else {
+            Toast.makeText(this, "Something went wrong...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
